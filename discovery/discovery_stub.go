db7324cd10e92c67fa35c0f707353153c3e2eaea
ThirteenR <793334058@qq.com>
2026-05-25 23:56:23 +0800

    fix: add registerDarwin stub for non-darwin platforms

--- discovery/discovery_stub.go
+++ discovery/discovery_stub.go
@@ -4,6 +4,10 @@ package discovery
 
 import "context"
 
+func registerDarwin(ctx context.Context, instance, uuid string, port int) (*RegisterHandle, error) {
+	return registerZeroconf(ctx, instance, uuid, "", port)
+}
+
 func discoverDarwin(ctx context.Context, handler Handler) error {
 	return discoverZeroconf(ctx, handler)
 }
